b8c18021bd0fa72e9b1668dbdde710b5e9a80f6a
Le Bui <lebui185@gmail.com>
2017-01-07 09:32:30 +0700

    Fix footer position

--- app/scripts/app.constant.js
+++ app/scripts/app.constant.js
@@ -1,5 +1,5 @@
 angular.module('orionEcommerceApp')
-	.constant('RESTAPI_URL', 'http://orionteam.esy.es/api')
+	.constant('API_ENDPOINT', 'http://orionteam.esy.es/api')
 	.constant('EVENT', {
 		CHECKOUT_STEP_LOADED: 'CHECKOUT_CART_LOADED'
 	});
